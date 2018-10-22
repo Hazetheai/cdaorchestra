d6b1693f9ff5a0870c04f2821c1f920b365d2c1e
Jake <jacobstefan92@gmail.com>
2018-10-22 14:16:31 +0100

    adding mobile menu

--- scripts.js
+++ scripts.js
@@ -12,6 +12,23 @@
   
 //   };
 
+// Hamburger
+const ham = document.querySelector('#nav-icon1');
+
+function toggleHam(){
+    ham.classList.toggle("open");
+}
+
+ham.addEventListener('click', toggleHam);
+
+
+// $(document).ready(function() {
+//     $("#nav-icon1").click(function() {
+//       $(this).toggleClass("open");
+//     });
+//   });
+
+
 const arrow = document.querySelector('.step-inside');
 const tag = document.querySelector('.tagline');
 
@@ -26,3 +43,33 @@ function hideArrow() {
 }   
 
 document.addEventListener('scroll', hideArrow);
+
+
+// Modal Image
+
+// Get the modal
+var modal = document.querySelectorAll('#myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelectorAll('#myImg');
+var modalImg = document.querySelectorAll("#img01");
+var captionText = document.querySelectorAll("#caption");
+img.forEach(el => el.onclick = function(){
+    modal.forEach(a => a.style.display = "block");
+    modalImg.forEach(b => b.src = this.src);
+    captionText.forEach(c => c.innerHTML = this.alt);
+    
+
+} )
+
+// Get the <span> element that closes the modal
+var span = document.querySelectorAll(".close");
+// console.log(span);
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.forEach(d => d.addEventListener('click', function close() { 
+    console.log("closed");
+    modal.forEach(a => a.style.display = "none");
+}));
